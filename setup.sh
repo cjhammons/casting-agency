f06d5f9d5e0dc64ee633518ad674d1a4d5cd9511
Curtis Hammons <curtishammons@gmail.com>
2020-08-23 14:07:37 -0700

    created new database directory and moved models into it

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-
+export DB_USERNAME='coolguy'
+export DB_PASSWORD='meme'
+export DB_URL='postgres://caxnpquxkgrwmb:92ee045aa796d634abc8d73d4759354a21c4cea909e332489cb6e1a61f59fe0d@ec2-18-235-109-97.compute-1.amazonaws.com:5432/deu6fg9lt94srj'
+export DB_NAME='casting-agency'
