79bd01d157c4400b971c2c7100927e3dda423c4f
Curtis Hammons <curtishammons@gmail.com>
2020-09-03 19:53:34 -0700

    updated env vars

--- database/models.py
+++ database/models.py
@@ -2,13 +2,14 @@ import os
 from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
-
+'''
 db_username = os.getenv('DATABASE_USERNAME')
 db_password = os.getenv('DATABASE_PASSWORD')
 db_url = os.getenv('DATABASE_URL')
 db_name = os.getenv('DATABASE_NAME')
 database_path = "postgres://{}:{}@{}/{}".format(db_username, db_password, db_url, db_name)
-#database_path = db_url
+'''
+database_path = os.getenv('DATABASE_URL')
 
 db = SQLAlchemy()
 
--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ export DATABASE_PASSWORD='meme'
 export DATABASE_URL='localhost:5432'
 export DATABASE_NAME='castingagency'
 export FLASK_APP=app.py
+export DATABASE_URL='postgres://coolguy:meme@localhost:5432/castingagency'
